8936c425438e989b7a5ecdc0fa1929324824ca9a
Kevin Zheng <kyzheng@umass.edu>
2023-05-23 23:48:04 -0400

    sketch of crawler

--- samples/crawl_from_video_id.py
+++ samples/crawl_from_video_id.py
@@ -1,2 +1,12 @@
-# to be run on command line
-# command line arguments: video_id, depth (<5)
+# from youtubetools.languageidentifier import identify_language
+#
+# print(identify_language("testvideoid_20230523_173130/wavs/AAKUqHBuzk4.wav", [1]))
+# print(classify_language_whisper("testvideoid_20230523_173130/wavs/AAKUqHBuzk4.wav"))
+
+
+# crawl, get tree
+# flatten tree -> queue/threaded
+#   download audio, metadata, transcripts
+#   youtubemusicsearch, insert into metadata file
+#   language_id, insert into metadata file
+# json to csv
