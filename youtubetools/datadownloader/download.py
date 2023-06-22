85cbaeb7a1e78e444a549732ef31eb15ced8c457
Kevin Zheng <kyzheng@umass.edu>
2023-06-22 14:03:46 -0400

    live video handling

--- youtubetools/datadownloader/download.py
+++ youtubetools/datadownloader/download.py
@@ -14,8 +14,9 @@ def download_data(collection, video_id, download_options=(True, True), audio_opt
     assert len(video_id) == 11, "video_id must be 11 characters long"
     if download_options[1]:  # download metadata and transcripts by default
         download_metadata_transcripts(collection, video_id, metadata_options)
-    with open(os.path.join(ROOT_DIR, "collections", collection, "metadata", f'{video_id}.json'), 'r') as md_file:
-        metadata = json.load(md_file)
-        if "is_live" in metadata.keys():
-            if not metadata["is_live"] and download_options[0]:  # download audio track by default, not live
-                download_audio_track(collection, video_id, audio_options)
+    if download_options[0]:
+        with open(os.path.join(ROOT_DIR, "collections", collection, "metadata", f'{video_id}.json'), 'r') as md_file:
+            metadata = json.load(md_file)
+        if "is_live" in metadata.keys() and metadata["is_live"]:  # download audio track by default, not live
+            return
+        download_audio_track(collection, video_id, audio_options)
