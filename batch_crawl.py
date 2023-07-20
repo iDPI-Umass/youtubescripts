4fb1c46e5a4bf63c681252cd0a419138b6d61908
Kevin Zheng <kyzheng@umass.edu>
2023-07-20 10:53:42 -0400

    Update batch_crawl.py

--- batch_crawl.py
+++ batch_crawl.py
@@ -1,16 +1,18 @@
 import argparse
 import subprocess
+import pandas as pd
 from queue import Queue
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", type=str, help="file location of csv")
 parser.add_argument("--skiplanguage", action="store_true", help="if true, skips language detection")
-args = parser.parse_args()
-print(args.file)
-print(args.skiplanguage)
 
+args = parser.parse_args()
+
+file_location = args.file
+df = pd.read_csv(file_location, header=None)
+root_ids = df[0].tolist()
 depth = 2
-root_ids = []
 
 q = Queue(maxsize=0)
 for root_id in root_ids:
@@ -18,6 +20,10 @@ for root_id in root_ids:
 while not q.empty():
     root_id = q.get()
     try:
-        subprocess.run(['python3', 'crawl_from_video_id.py', f'\"{root_id}\"'], depth)
+        query = ['python3', 'crawl_from_video_id.py', f'\"{root_id}\"', depth]
+        if args.skiplanguage:
+            query.append('--skiplanguage')
+        subprocess.run(query)
     except Exception as e:
         print(e)
+    q.task_done()
